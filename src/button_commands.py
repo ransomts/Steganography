9cc7ccbaa69766943441af416c7afdd22a40e965
Tim Ransom <ransomts@appstate.edu>
2016-03-22 22:13:37 -0400

    Finishing this up, got most everything in

--- src/button_commands.py
+++ src/button_commands.py
@@ -7,7 +7,7 @@ from decrypt import *
 
 def how_to_use_command():
     showinfo('How to use', "<fill in later>")
-    
+
 def about_command():
     showinfo('About', 'Author: Tim Ransom\nMarch 2016')
 
@@ -43,23 +43,26 @@ def open_decode_image(right_image, left_image, right_image_filename):
 
         left_image.configure(image='', text='Image output will be displayed in the text area')
 
-def push_decode_image(filename):
+def push_decode_image(filename, right_image):
     new_encode_image= PhotoImage(Image.open(filename))
     right_image.configure(image = new_encode_image)
     right_image.image = new_encode_image
-            
-def encode_button_command(encode_image, decode_image, text_area, encode_image_filename):
-    push_decode_image(encode_text(text_area.get('1.0', END), encode_image))
-    showinfo('Encode', 'Encoding Complete\n' + str(encode_image.image_types()))
 
-def decode_button_command(right_image, text_area, decode_variable):
+def encode_button_command(encode_image, decode_image, text_area, left_image_filename, right_image_filename):
+    if left_image_filename.get() != () and left_image_filename.get() != '':
+        push_decode_image(encode_text(text_area.get('1.0', END), left_image_filename.get()), decode_image)
+        showinfo('Encode', 'Encoding Complete\n' + left_image_filename.get())
+
+def decode_button_command(right_image, text_area, decode_variable, right_image_filename):
     if decode_variable.get() == 'Text Area':
         text_area.delete('1.0', END)
-        text_area.insert(END, decode_text(decode_image_filename))
+        if right_image_filename.get() != '':
+            text_area.insert(END, decode_text(right_image_filename.get()))
     else:
-        message = decode_text(decode_image_filename)
-        new_filepath = askopenfilename()
+        message = decode_text(right_image_filename.get())
+        new_filepath = asksaveasfilename()
         if new_filepath != () and new_filepath != '':
             with open(new_filepath, 'w') as new_file:
                 new_file.write(message)
-    showinfo('Decode', 'Decoding Complete')
+    if right_image_filename.get() != '':
+        showinfo('Decode', 'Decoding Complete\nRead File: ' + right_image_filename.get())
