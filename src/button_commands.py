5dfa4606d345459a008a9e7dd7e7d2246c6fe17b
Tim Ransom <ransomts@appstate.edu>
2016-03-22 20:09:15 -0400

    Fixed inconsistant quotes...

--- src/button_commands.py
+++ src/button_commands.py
@@ -6,10 +6,10 @@ from encrypt import *
 from decrypt import *
 
 def how_to_use_command():
-    showinfo("How to use", "<fill in later>")
+    showinfo('How to use', "<fill in later>")
     
 def about_command():
-    showinfo("About", 'Author: Tim Ransom\nMarch 2016')
+    showinfo('About', 'Author: Tim Ransom\nMarch 2016')
 
 def quit_command(master):
     master.destroy()
@@ -21,20 +21,22 @@ def read_text(text_area, encode_variable):
     if filename != () and filename != '':
         with open(filename, 'r') as plaintext_file:
             text_area.insert('1.0', plaintext_file.read())
-    encode_variable.set("Text Area")
+    encode_variable.set('Text Area')
 
-def open_encode_image(left_image, right_image):
+def open_encode_image(left_image, right_image, left_image_filename):
     filename = askopenfilename()
     if filename != () and filename != '':
+        left_image_filename.set(filename)
         new_encode_image= PhotoImage(Image.open(filename))
         left_image.configure(image = new_encode_image)
         left_image.image = new_encode_image
 
         right_image.configure(text='The encoded image will be displayed here after encode is hit',image='')
 
-def open_decode_image(right_image, left_image):
+def open_decode_image(right_image, left_image, right_image_filename):
     filename = askopenfilename()
     if filename != () and filename != '':
+        right_image_filename.set(filename)
         new_encode_image= PhotoImage(Image.open(filename))
         right_image.configure(image = new_encode_image)
         right_image.image = new_encode_image
@@ -47,12 +49,12 @@ def push_decode_image(filename):
     right_image.image = new_encode_image
             
 def encode_button_command(encode_image, decode_image, text_area, encode_image_filename):
-    push_decode_image(encode_text(text_area.get("1.0", END), encode_image))
-    showinfo("Encode", "Encoding Complete\n" + str(encode_image.image_types()))
+    push_decode_image(encode_text(text_area.get('1.0', END), encode_image))
+    showinfo('Encode', 'Encoding Complete\n' + str(encode_image.image_types()))
 
 def decode_button_command(right_image, text_area, decode_variable):
     if decode_variable.get() == 'Text Area':
-        text_area.delete("1.0", END)
+        text_area.delete('1.0', END)
         text_area.insert(END, decode_text(decode_image_filename))
     else:
         message = decode_text(decode_image_filename)
@@ -60,4 +62,4 @@ def decode_button_command(right_image, text_area, decode_variable):
         if new_filepath != () and new_filepath != '':
             with open(new_filepath, 'w') as new_file:
                 new_file.write(message)
-    showinfo("Decode", "Decoding Complete")
+    showinfo('Decode', 'Decoding Complete')
--- src/main_window.py
+++ src/main_window.py
@@ -5,38 +5,39 @@ from ImageTk import *
 from tkMessageBox import *
 from button_commands import *
 
-
+##################################################
+# Sets up the main window frame with the title   #
+# and the grid system                            #
+##################################################
 master = Tk()
-# Sets window title
-master.wm_title('Stegenography')
 
-# Sets up the grid weights to align the
-#   objects with
+master.wm_title('Stegenography')
 master.columnconfigure(0, weight=2)
 master.columnconfigure(1, weight=1)
 master.columnconfigure(2, weight=1)
 master.rowconfigure(0, weight=1)
 master.rowconfigure(1, weight=1)
 
-# Open up a couple images to put in for placeholders
-encode_image = PhotoImage(file="../images/cat.png")
-decode_image = PhotoImage(Image.open("../images/cat.png"))
-
 ##################################################
 # Labels to display the images in, when clicked, #
 # opens dialogs to set up to encode or decode    #
 # that image.                                    #
 ##################################################
-left_image = Label(master, text="Open File to kickoff", #image=encode_image,
+left_image_filename = StringVar()
+right_image_filename = StringVar()
+left_image_filename.set('')
+right_image_filename.set('')
+
+left_image = Label(master, text='Open File to kickoff',
                    anchor=W, justify=LEFT, wraplength=200, cursor='hand1')
 left_image.grid(row=0, column=0, sticky=W+E+N+S)
 
-right_image = Label(master, text="Second", #image=decode_image,
+right_image = Label(master, text='',
                     anchor=W, justify=LEFT, wraplength=200, cursor='hand1')
 right_image.grid(row=0, column=1, columnspan=2, sticky=W+E+N+S)
 
-right_image.bind("<Button-1>", lambda e: open_decode_image(right_image, left_image))
-left_image.bind("<Button-1>", lambda e: open_encode_image(left_image, right_image))
+left_image.bind('<Button-1>', lambda e: open_encode_image(left_image, right_image, left_image_filename))
+right_image.bind('<Button-1>', lambda e: open_decode_image(right_image, left_image, right_image_filename))
 
 ##################################################
 # Text area to display or load in text to or     #
@@ -54,7 +55,7 @@ encode_lableframe = LabelFrame(master, text='Encode Input')
 encode_lableframe.grid(row = 2, column = 1, columnspan=2, sticky=W+E+N+S)
 
 encode_variable = StringVar()
-encode_variable.set("Text Area")
+encode_variable.set('Text Area')
 
 encode_rb_1 = Radiobutton(encode_lableframe, text='Text Area',
                           variable=encode_variable, value="Text Area")
@@ -66,7 +67,7 @@ encode_rb_2 = Radiobutton(encode_lableframe, text='File',
 encode_rb_2.grid(row=1,column=0, sticky=W)
 
 decode_variable = StringVar()
-decode_variable.set("Text Area")
+decode_variable.set('Text Area')
 decode_lableframe = LabelFrame(master, text='Decode Output')
 decode_lableframe.grid(row = 3, column = 1, columnspan=2, sticky=W+E+N+S)
 
@@ -102,17 +103,15 @@ filemenu.add_command(label='Read text from file', command=lambda: read_text(text
 filemenu.add_command(label='Open image to encode', command=lambda : open_encode_image(left_image, right_image))
 filemenu.add_command(label='Open image to decode', command=lambda : open_decode_image(right_image, left_image))
 
-
 helpmenu = Menu(menubar, tearoff=False)
 helpmenu.add_command(label='How to use', command=how_to_use_command)
 helpmenu.add_command(label='About', command=about_command)
-helpmenu.add_command(label='Quit', command=lambda e:quit_command(), underline=0, accelerator="C-q")
-
+helpmenu.add_command(label='Quit', command=lambda e:quit_command(), underline=0, accelerator='C-q')
 
 menubar.add_cascade(label='File', menu=filemenu)
 menubar.add_cascade(label='Help', menu=helpmenu)
 
-master.bind("<Control-q>", lambda e: quit_command(master))
+master.bind('<Control-q>', lambda e: quit_command(master))
 master.config(menu=menubar)
 
 ##################################################
